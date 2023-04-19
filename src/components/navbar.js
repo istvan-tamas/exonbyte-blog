96be73fd45e38340352c21dce73be696880db463
istvan-tamas <istvan.tamas90@gmail.com>
2023-04-19 12:48:59 +0200

    Basic navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,9 +6,21 @@ export default function Navbar() {
 		<nav>
 			<h2>exonbyte devblog</h2>
 			<ul>
-				<li></li>
-				<li></li>
-				<li></li>
+				<li>
+					<Link to="/">home</Link>
+				</li>
+				<li>
+					<Link to="/about">about</Link>
+				</li>
+				<li>
+					<Link to="/projects">projects</Link>
+				</li>
+				<li>
+					<Link to="/blog">blog</Link>
+				</li>
+				<li>
+					<Link to="/projects">github</Link>
+				</li>
 			</ul>
 		</nav>
 	);
