75b37aa53c834104b45afb103f4c115d0eb9aa35
istvan-tamas <istvan.tamas90@gmail.com>
2023-04-19 12:45:48 +0200

    Added basic navigation elements

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,15 @@
 import { Link } from 'gatsby';
 import React from 'react';
 
-export default function Navbar() {}
+export default function Navbar() {
+	return (
+		<nav>
+			<h2>exonbyte devblog</h2>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</nav>
+	);
+}
