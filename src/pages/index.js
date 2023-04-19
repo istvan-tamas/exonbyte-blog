6f8e3023d22bb678fb75373219c0f68644b11c17
istvan-tamas <istvan.tamas90@gmail.com>
2023-04-19 12:53:07 +0200

    Added the rest of the pages to the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import * as React from 'react';
+import Navbar from '../components/navbar';
 
 export default function Home() {
-	return <h1>Hello world!</h1>;
+	return (
+		<div>
+			<h1>Hello world!</h1>
+			<Navbar />
+		</div>
+	);
 }
