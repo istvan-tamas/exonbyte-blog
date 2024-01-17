0a6c0102679ad1e4c7853b534a589135bd6debab
istvan-tamas <istvan.tamas90@gmail.com>
2024-01-17 13:31:20 +0100

    index update

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,15 +14,7 @@ export default function Home({ data }) {
 					<h3>
 						{title} - {description}
 					</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-						culpa qui officia deserunt mollit anim id est laborum.
-					</p>
+					<p>Welcome to exonybte.dev!</p>
 					<Link className={styles.btn} to="/projects">
 						Current projects
 					</Link>
