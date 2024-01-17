21ac96791551b8a085feca7a43cc8e01ceb837cb
istvan-tamas <istvan.tamas90@gmail.com>
2024-01-17 13:40:58 +0100

    blog update

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,15 +6,7 @@ export default function Home() {
 		<Layout>
 			<div>
 				<h3>Current blog posts</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-					culpa qui officia deserunt mollit anim id est laborum.
-				</p>
+				<p>There are no blog posts yet!</p>
 			</div>
 		</Layout>
 	);
